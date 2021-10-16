726bc784afa0711669955883d8b332cc76a44d26
coffee <164589895@qq.com>
2021-10-16 08:56:50 +0800

    1

---
+++ src/ExcelOut.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Topfunction\Topfunction;
+
+/**
+ * ExcelOut
+ * Class ExcelOut
+ * @package Topfunction\Topfunction
+ */
+class ExcelOut
+{
+    public $file      = '';     // 文件名
+    public $title     = '';     // 表名
+    public $head      = [];     // 表头
+    public $data_list = [];     // 数据
+
+}
