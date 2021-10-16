f4b61ff568530f70b86ddea178b276945a7ff912
coffee <164589895@qq.com>
2021-10-16 10:44:44 +0800

    1

---
+++ demo/ExcelOut.php
@@ -0,0 +1,37 @@
+<?php
+
+require "../vendor/autoload.php";
+
+$a        = new \Topfunction\Topfunction\ExcelOut();
+$a->head  = [
+    'name' => '姓名',
+    'sex'  => '性别（男女）',
+    'tel'  => '电话'
+];
+$a->list  = [
+    [
+        'name' => '小名',
+        'sex'  => '男'
+    ],
+    [
+        'name' => '小名1',
+        'sex'  => '男1'
+    ],
+    [
+        'sex'  => '男2',
+        'name' => '小名2',
+
+    ],
+    [
+        'name' => '小名3',
+        'tel'  => '13520476002',
+
+    ],
+    [
+        'sex' => '男4',
+
+    ]
+];
+$a->file  = '通寻路';
+$a->title = '通寻路';
+print_r($a->save());
